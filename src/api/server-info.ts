76ae8c20ea82f33dbfc8f769255fa447a1cb62ec
Burrito <burritoyangster@gmail.com>
2024-01-12 10:24:05 +0800

    Cleaned up code

--- src/api/server-info.ts
+++ src/api/server-info.ts
@@ -1,4 +1,4 @@
-import { Database, LocalizationText, ServerInfo } from 'sonolus-core'
+import { Database, LocalizationText, Section, ServerInfo } from 'sonolus-core'
 import {
     SearchInfo,
     toBackgroundItem,
@@ -9,6 +9,7 @@ import {
     toSearch,
     toSkinItem,
 } from '.'
+import { ToItem } from './item'
 import { toReplayItem } from './replay-item'
 
 export const toServerInfo = (
@@ -25,32 +26,28 @@ export const toServerInfo = (
 ): ServerInfo => ({
     title: localize(serverInfo.info.title),
     banner: serverInfo.info.banner,
-    levels: {
-        items: serverInfo.levels.map((info) => toLevelItem(db, localize, info)),
-        search: toSearch(localize, levelsSearch),
-    },
-    skins: {
-        items: serverInfo.skins.map((info) => toSkinItem(db, localize, info)),
-        search: toSearch(localize, skinsSearch),
-    },
-    backgrounds: {
-        items: serverInfo.backgrounds.map((info) => toBackgroundItem(db, localize, info)),
-        search: toSearch(localize, backgroundsSearch),
-    },
-    effects: {
-        items: serverInfo.effects.map((info) => toEffectItem(db, localize, info)),
-        search: toSearch(localize, effectsSearch),
-    },
-    particles: {
-        items: serverInfo.particles.map((info) => toParticleItem(db, localize, info)),
-        search: toSearch(localize, particlesSearch),
-    },
-    engines: {
-        items: serverInfo.engines.map((info) => toEngineItem(db, localize, info)),
-        search: toSearch(localize, enginesSearch),
-    },
-    replays: {
-        items: serverInfo.replays.map((info) => toReplayItem(db, localize, info)),
-        search: toSearch(localize, replaysSearch),
-    },
+    levels: toSection(db, localize, serverInfo.levels, toLevelItem, levelsSearch),
+    skins: toSection(db, localize, serverInfo.skins, toSkinItem, skinsSearch),
+    backgrounds: toSection(
+        db,
+        localize,
+        serverInfo.backgrounds,
+        toBackgroundItem,
+        backgroundsSearch,
+    ),
+    effects: toSection(db, localize, serverInfo.effects, toEffectItem, effectsSearch),
+    particles: toSection(db, localize, serverInfo.particles, toParticleItem, particlesSearch),
+    engines: toSection(db, localize, serverInfo.engines, toEngineItem, enginesSearch),
+    replays: toSection(db, localize, serverInfo.replays, toReplayItem, replaysSearch),
+})
+
+const toSection = <T, U>(
+    db: Database,
+    localize: (text: LocalizationText) => string,
+    infos: T[],
+    toItem: ToItem<T, U>,
+    search: SearchInfo,
+): Section<U> => ({
+    items: infos.map((info) => toItem(db, localize, info)),
+    search: toSearch(localize, search),
 })
