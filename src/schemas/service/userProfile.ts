94f1da4baa626525a912aac688a6ca3545020d20
Burrito <burritoyangster@gmail.com>
2025-06-27 21:10:17 +0800

    Updated service user profile

--- src/schemas/service/userProfile.ts
+++ src/schemas/service/userProfile.ts
@@ -7,8 +7,12 @@ export const serviceUserProfileSchema = Type.Object({
     id: Type.Unsafe<ServiceUserId>(Type.String()),
     handle: Type.String(),
     name: Type.String(),
+    avatarType: Type.String(),
+    avatarForegroundType: Type.String(),
     avatarForegroundColor: Type.String(),
+    avatarBackgroundType: Type.String(),
     avatarBackgroundColor: Type.String(),
+    bannerType: Type.String(),
     aboutMe: Type.String(),
     favorites: Type.Array(Type.String()),
 })
