3570cc4a061101c188e0a2a6d6997812f6ce0ebf
NonSpicyBurrito <burritoyangster@gmail.com>
2026-01-22 07:51:41 +0800

    Added quick search value to list

--- src/models/server/items/list.ts
+++ src/models/server/items/list.ts
@@ -3,12 +3,16 @@ import { SonolusBase } from '../../../sonolus/base.js'
 import { Localize } from '../../../utils/localization.js'
 import { ToItem, toItems } from '../../items/item.js'
 import { PickForms, ServerFormsModel, toServerForms } from '../../server/forms/form.js'
+import { RawServerFormValue, serializeRawServerFormsValue } from '../forms/value.js'
 
 export type ServerItemListModel<TItemModel, TSearches extends ServerFormsModel> = {
     pageCount: number
     cursor?: string
     items: TItemModel[]
     searches?: PickForms<TSearches>
+    quickSearchValue?: {
+        [K in keyof TSearches]: RawServerFormValue<K & string, TSearches[K]>
+    }[keyof TSearches]
 }
 
 export const toServerItemList = <TItemModel, TSearches extends ServerFormsModel, TItem>(
@@ -22,4 +26,6 @@ export const toServerItemList = <TItemModel, TSearches extends ServerFormsModel,
     cursor: list.cursor,
     items: toItems(sonolus, localize, toItem, list.items),
     searches: list.searches && toServerForms(localize, list.searches, searches),
+    quickSearchValues:
+        list.quickSearchValue && serializeRawServerFormsValue(list.quickSearchValue, searches),
 })
