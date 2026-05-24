4e32d8a6bb93c793d4a5c9849345ac6624857a85
NonSpicyBurrito <burritoyangster@gmail.com>
2026-05-25 07:32:15 +0800

    Added and refactored to use server info button model

--- src/models/server/info.ts
+++ src/models/server/info.ts
@@ -1,11 +1,79 @@
-import { LocalizationText, ServerInfo, ServerInfoButton, Srl } from '@sonolus/core'
+import { Icon, LocalizationText, ServerInfo, ServerInfoButton, Srl } from '@sonolus/core'
 import { Localize } from '../../utils/localization.js'
 import { PickOptions, ServerOptionsModel, toServerOptions } from './options/option.js'
+
+export type ServerInfoButtonModel =
+    | ServerInfoAuthenticationButtonModel
+    | ServerInfoItemButtonModel
+    | ServerInfoConfigurationButtonModel
+
+export type ServerInfoAuthenticationButtonModel = {
+    type: 'authentication'
+}
+
+export type ServerInfoItemButtonModel = {
+    type:
+        | 'room'
+        | 'post'
+        | 'playlist'
+        | 'level'
+        | 'replay'
+        | 'skin'
+        | 'background'
+        | 'effect'
+        | 'particle'
+        | 'engine'
+        | 'user'
+    title?: LocalizationText
+    icon?: Icon
+    badgeCount?: number
+    infoType?: string
+    itemName?: string
+}
+
+export type ServerInfoConfigurationButtonModel = {
+    type: 'configuration'
+}
+
+const toServerInfoButton = (
+    localize: Localize,
+    serverInfoButton: ServerInfoButtonModel,
+): ServerInfoButton => {
+    switch (serverInfoButton.type) {
+        case 'authentication':
+            return {
+                type: 'authentication',
+            }
+        case 'room':
+        case 'post':
+        case 'playlist':
+        case 'level':
+        case 'replay':
+        case 'skin':
+        case 'background':
+        case 'effect':
+        case 'particle':
+        case 'engine':
+        case 'user':
+            return {
+                type: serverInfoButton.type,
+                title: serverInfoButton.title && localize(serverInfoButton.title),
+                icon: serverInfoButton.icon,
+                badgeCount: serverInfoButton.badgeCount,
+                infoType: serverInfoButton.infoType,
+                itemName: serverInfoButton.itemName,
+            }
+        case 'configuration':
+            return {
+                type: 'configuration',
+            }
+    }
+}
 
 export type ServerInfoModel<T extends ServerOptionsModel> = {
     title: LocalizationText
     description?: LocalizationText
-    buttons: ServerInfoButton[]
+    buttons: ServerInfoButtonModel[]
     configuration: {
         options: PickOptions<T>
     }
@@ -19,7 +87,7 @@ export const toServerInfo = <T extends ServerOptionsModel>(
 ): ServerInfo => ({
     title: localize(serverInfo.title),
     description: serverInfo.description && localize(serverInfo.description),
-    buttons: serverInfo.buttons,
+    buttons: serverInfo.buttons.map((button) => toServerInfoButton(localize, button)),
     configuration: {
         options: toServerOptions(localize, serverInfo.configuration.options, options),
     },
