b879624406d4afb4463174511ee30f7ce24dd154
Burrito <burritoyangster@gmail.com>
2026-05-25 07:39:29 +0800

    Add and refactor to use server info button model (#51)

--- src/models/server/index.ts
+++ src/models/server/index.ts
@@ -1,4 +1,10 @@
 export * from './forms/index.js'
-export { ServerInfoModel } from './info.js'
+export {
+    ServerInfoAuthenticationButtonModel,
+    ServerInfoButtonModel,
+    ServerInfoConfigurationButtonModel,
+    ServerInfoItemButtonModel,
+    ServerInfoModel,
+} from './info.js'
 export * from './items/index.js'
 export * from './options/index.js'
